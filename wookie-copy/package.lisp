5314839f14fa23506a58237f279202c3e4a8c077
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-11-05 17:31:06 +0900

    update wookie

--- wookie-copy/package.lisp
+++ wookie-copy/package.lisp
@@ -1,6 +1,8 @@
 (defpackage :wookie-copy
-  (:use :cl)
-  (:export #:defroute
+  (:use :cl :asdf)
+  (:export #:clear-routes
+           #:clear-route
+           #:defroute
            #:acceptor
            #:start-server
            #:send-reply))
