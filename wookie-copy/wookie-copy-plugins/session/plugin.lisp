be5f1c0518487d65acb3110c39a5cc23c846371a
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-11-12 16:02:19 +0900

    update session/plugin.lisp

--- wookie-copy/wookie-copy-plugins/session/plugin.lisp
+++ wookie-copy/wookie-copy-plugins/session/plugin.lisp
@@ -7,11 +7,15 @@
   (wookie-plugin:set-plugin-request-data :session request "omg"))
 
 (defun init-session ()
-  (wookie-copy:add-hook :pre-route #'load-session-data))
+  (wookie-copy:add-hook :pre-route #'load-session-data :session-main-hook))
+
+(defun unload-session ()
+  (wookie-copy:remove-hook :pre-route :session-main-hook))
 
 (wookie-plugin:register-plugin
  :session
  '(:name "Wookie session handler"
    :authore "Andrew Lyon"
    :version "0.1.0")
- #'init-session)
+ #'init-session
+ #'unload-session)
