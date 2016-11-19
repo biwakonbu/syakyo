c273a8457d6547520d2551c337d4e9d292ab148f
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-11-19 21:08:56 +0900

    add wookie-copy-plugins

---
+++ wookie-copy/wookie-copy-plugins/get/plugin.lisp
@@ -0,0 +1,31 @@
+(defpackage :wookie-core-get-vars
+  (:use :cl :wookie-core-get-vars))
+(in-package :wookie-core-get-vars)
+
+(defun parse-get-vars (request)
+  "Grab GET data from parsed URI querystring and set into a hash table stored
+   with the request."
+  (let ((hash-get-vars (make-hash-table :test #'equal)))
+    (map-querystring (puri:uri-query (request-uri request))
+      (lambda (key val)
+        (setf (gethash key hash-get-vars) val)))
+    (wookie-plugin:set-plugin-request-data :get request hash-get-vars)))
+
+(defplugfun get-var (request key)
+  "Get a value from the GET data by key."
+  (let ((hash-get-vars (wookie-copy-plugin:get-plugin-request-data :get request)))
+    (gethash key hash-get-vars)))
+
+(defun init-get-vars ()
+  (wookie-copy:add-hook :parsed-headers #'parse-get-vars :get-core-parse-vars))
+
+(defun unload-get-vars ()
+  (wookie-copy:remove-hook :parsed-headers :get-core-parse-vars))
+
+(wookie-copy-plugin:register-plugin
+ :get
+ '(:name "Wookie core GET plugin"
+   :author "Andrew Lyon"
+   :version "0.1.0")
+ #'init-get-vars
+ #'unload-get-vars)
---
+++ wookie-copy/wookie-copy-plugins/post/plugin.lisp
@@ -0,0 +1,52 @@
+(defpackage :wookie-copy-core-post-vars
+  (:use :cl :wookie :wookie-util :wookie-plugin))
+(in-package :wookie-copy-core-post-vars)
+
+(defun check-if-post (request)
+  "Check if this request contains POST data, and mark the plugin data as such so
+   once we have body data we know whether or not to try and parse it."
+  (let ((headers (request-headers request)))
+    (when (search "application/x-www-form-urlencoded" (getf headers :content-type))
+      ;; make sure we store the body so we can access it uploads
+      (setf (http-parse:http-store-body (request-http request)) t)
+      ;; setting T here lets the `parse-post-bars` fn know that we're dealing
+      ;; with POST vars
+      (wookie-copy-plugin:set-plugin-request-data :post request t))))
+
+(defun parse-post-vars (request)
+  "Grab POST data from parsed URI querystring and set into a hash table stored
+   with the request."
+  (when (wookie-copy-plugin:get-plugin-request-data :post request)
+    ;; convert the body to a string via the Content-Type header
+    (let* ((hash-post-vars (make-hash-table :test #'equal))
+           (headers (request-headers request))
+           (body (body-to-string (http-parse:http-body (request-http request))
+                                 (getf headers :content-type))))
+      ;; IF the body is a querystring, parse it and set into the POST hash
+      (when (querystringp body)
+        (map-querystring body
+                         (lambda (key val)
+                           (setf (gethash key hash-post-vars) val)))
+        (wookie-copy-plugin:set-plugin-request-data :post request hash-post-vars)))))
+
+(defplugfun post-var (request key)
+  "Get a value from the POST data by key."
+  (let ((hash-post-vars (wookie-copy-plugin:get-plugin-request-data :post request)))
+    (gethash key hash-post-vars)))
+
+(defun init-post-vars ()
+  (wookie-copy:add-hook :parsed-headers #'check-if-post :post-core-check-post)
+  (wookie-copy:add-hook :body-complete #'parse-post-vars :opst-core-parse-post))
+
+(defun unload-post-vars ()
+  (wookie-copy:remove-hook :parsed-headers :post-core-plugin)
+  (wookie-copy:remove-hook :body-complete :post-core-parse-post))
+
+(wookie-plugin:register-plugin
+ :post
+ '(:name "Wookie core POST plugin"
+   :author "Andrew Lyon"
+   :version "0.1.0")
+ #'init-post-vars
+ #'unload-post-vars)
+
