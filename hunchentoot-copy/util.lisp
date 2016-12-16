cf010a378276801e184ab9480f5f1038f94feb8c
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2016-12-16 18:59:25 +0900

    add escape-for-html func

--- hunchentoot-copy/util.lisp
+++ hunchentoot-copy/util.lisp
@@ -74,3 +74,17 @@ THING is a string or a symbol.")
     (loop for code across (md5:md5sum-string string)
           do (format s "~2,'0x" code))))
 
+(defun escape-for-html (string)
+  "Escapes the characters #\\<, #\\>, #\\', #\\\", and #\\& for HTML
+output."
+  (with-output-to-string (out)
+    (with-input-from-string (in string)
+      (loop for char = (read-char in nil nil)
+            while char
+            do (case char
+                 ((#\<) (write-string "&lt;" out))
+                 ((#\>) (write-string "&gt;" out))
+                 ((#\") (write-string "&quot;" out))
+                 ((#\') (write-string "&#039;" out))
+                 ((#\&) (write-string "&amp;" out))
+                 (otherwise (write-char char out)))))))
