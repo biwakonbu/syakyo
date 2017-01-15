b4c409fc382a5cc3366d0d9757a237cb24196889
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-01-15 18:23:11 +0900

    add mainloop func

--- nwe/src/nwe.lisp
+++ nwe/src/nwe.lisp
@@ -17,6 +17,45 @@
            (funcall function))
       (display-finalize))))
 
+;; main loop
+(defun nwe-mainlopp ()
+  (macrolet ((form (&body body)
+               `(cond (*debug-p*
+                       (handler-bind ((error #'bailout)
+                                      #+sbcl (sb-sys:interactive-interrupt #'bailout))
+                         ,@body))
+                      (t
+                       ,@body))))
+    (do-commandloop (:toplevel t)
+      (with-error-handler ()
+        (form
+         (syntax-scan-lines (current-buffer)
+                            (current-linum)
+                            (1+ (current-linum)))
+         (redraw-display)
+         (let ()
+           (start-idle-timers)
+           (let ((cmd (read-key-command)))
+             (stop-idle-timers)
+             (if (changed-disk-p (current-buffer))
+                 (ask-revert-buffer)
+                 (progn
+                   (message nil)
+                   (handler-case
+                       (handler-case
+                           (handler-bind ((editor-condition
+                                           (lambda (c)
+                                             (declare (ignore c))
+                                             (stop-record-key))))
+                             (cmd-call cmd nil))
+                         (editor-abort ()
+                           (buffer-mark-cancel (current-buffer))
+                           (message "Quit"))
+                         (read-only-error ()
+                           (message "Read Only"))
+                         (editor-error (c)
+                           (message (editor-error-message)))))))))))))
+
 (defmacro with-editor (() &body body)
   `(call-with-editor (lambda () ,@body)))
 
