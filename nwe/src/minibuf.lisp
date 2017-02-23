41d8a238b9c22246f6a7eb22cb0e9d296fa77bce
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-02-23 19:44:05 +0900

    add functions

--- nwe/src/minibuf.lisp
+++ nwe/src/minibuf.lisp
@@ -7,7 +7,12 @@
 (defvar *minibuffer-start-point*)
 (defvar minibuffer-prompt-attribute* (make-attribute "blue" nil :bold-p t))
 
+(defun minibuffer-window () *minibuf-window*)
+(defun minibuffer-window-p (window) (eq window (minibuffer-window)))
+(defun minibuffer-window-active-p () (eq (current-window) (minibuffer-window)))
 (defun minibuffer-window-height () *minibuffer-window-height*)
+(defun minibuffer () (window-buffer (minibuffer-window)))
+(defun minibuffer-calls-window () *minibuffer-calls-window*)
 
 (defun minibuf-init ()
   (let* ((buffer (make-buffer " *minibuffer*"))
