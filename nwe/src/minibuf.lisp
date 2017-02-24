72da08d3d50f48e0ba95da727113aeae5d8a0284
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2017-02-24 18:05:49 +0900

    add exports

--- nwe/src/minibuf.lisp
+++ nwe/src/minibuf.lisp
@@ -1,5 +1,23 @@
 (in-package :nwe)
 
+(export '(minibuffer-window-p
+          minibuffer-window-height
+          message
+          minibuf-y-or-n-p
+          minibuf-read-char
+          *minibuf-read-keymap*
+          active-minibuffer-window
+          check-switch-minibuffer-window
+          minibuf-read-line-confirm
+          minibuf-read-line-completion
+          minibuf-read-line-prev-history
+          minibuf-read-line-next-history
+          minibuf-read-line
+          minibuf-read-string
+          minibuf-read-number
+          minibuf-read-buffer
+          minibuf-read-file))
+
 (defparameter *minibuffer-window-height* 1)
 
 (defvar *minibuf-window*)
